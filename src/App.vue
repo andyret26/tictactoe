6a954766e28b7f0d576c0a9dd29c710b003cbb5e
Anders <60463942+andyret26@users.noreply.github.com>
2021-03-22 16:34:18 +0100

    init the vue.js app

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,222 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <h1>Tic Tac Toe</h1>
+    <p>{{player}}</p>
+    <p>{{msg}}</p>
+    <div class="container">
+      
+      <box 
+      class="box" 
+      v-for="num in list" 
+      :key="num"
+      :boxNum='num'
+      :round='round'
+      @boxClicked='doThis'
+      >
+      </box>
+
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import box from './components/Box'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    box
+  },
+
+  data: function(){
+    return {
+      msg: '',
+      list: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+      round: 1,
+      player: "player 1's turn",
+      player1Clicks: [],
+      player2Clicks: []
+    }
+  },
+
+  methods:{
+    doThis: function(boxNum){
+      this.round += 1
+      if(this.round%2 === 0){
+        this.player = "player 2's turn"
+        this.player1Clicks.push(boxNum)
+        this.winConP1()
+        
+
+      }
+      else{
+        this.player = "player 1's turn"
+        this.player2Clicks.push(boxNum)
+        this.winConP2()
+      }
+      
+      if(this.round === 10){
+        this.msg = 'IT IS A DRAW'
+      }
+
+    },
+
+    // WIN CODITION FOR PLAYER1
+    winConP1: function(){
+      if(this.player1Clicks.includes(0) 
+      && this.player1Clicks.includes(1) 
+      && this.player1Clicks.includes(2))
+      {
+        this.msg = 'PLAYER 1 Won'
+      }
+      else if (this.player1Clicks.includes(3) 
+      && this.player1Clicks.includes(4) 
+      && this.player1Clicks.includes(5))
+      {
+        this.msg = 'PLAYER 1 Won'
+      }
+      else if (this.player1Clicks.includes(6) 
+      && this.player1Clicks.includes(7) 
+      && this.player1Clicks.includes(8))
+      {
+        this.msg = 'PLAYER 1 Won'
+      }
+
+
+      else if (this.player1Clicks.includes(0) 
+      && this.player1Clicks.includes(3) 
+      && this.player1Clicks.includes(6))
+      {
+        this.msg = 'PLAYER 1 Won'
+      }
+      else if (this.player1Clicks.includes(1) 
+      && this.player1Clicks.includes(4) 
+      && this.player1Clicks.includes(7))
+      {
+        this.msg = 'PLAYER 1 Won'
+      }
+      else if (this.player1Clicks.includes(2) 
+      && this.player1Clicks.includes(5) 
+      && this.player1Clicks.includes(8))
+      {
+        this.msg = 'PLAYER 1 Won'
+      }
+
+
+      else if (this.player1Clicks.includes(0) 
+      && this.player1Clicks.includes(4) 
+      && this.player1Clicks.includes(8))
+      {
+        this.msg = 'PLAYER 1 Won'
+      }
+      else if (this.player1Clicks.includes(2) 
+      && this.player1Clicks.includes(4) 
+      && this.player1Clicks.includes(6))
+      {
+        this.msg = 'PLAYER 1 Won'
+      }
+    },
+
+
+    // WIN CONDITION FOR PLAYER 2
+    winConP2: function(){
+      if(this.player2Clicks.includes(0) 
+      && this.player2Clicks.includes(1) 
+      && this.player2Clicks.includes(2))
+      {
+        this.msg = 'PLAYER 2 Won'
+      }
+      else if (this.player2Clicks.includes(3) 
+      && this.player2Clicks.includes(4) 
+      && this.player2Clicks.includes(5))
+      {
+        this.msg = 'PLAYER 2 Won'
+      }
+      else if (this.player2Clicks.includes(6) 
+      && this.player2Clicks.includes(7) 
+      && this.player2Clicks.includes(8))
+      {
+        this.msg = 'PLAYER 2 Won'
+      }
+
+
+      else if (this.player2Clicks.includes(0) 
+      && this.player2Clicks.includes(3) 
+      && this.player2Clicks.includes(6))
+      {
+        this.msg = 'PLAYER 2 Won'
+      }
+      else if (this.player2Clicks.includes(1) 
+      && this.player2Clicks.includes(4) 
+      && this.player2Clicks.includes(7))
+      {
+        this.msg = 'PLAYER 2 Won'
+      }
+      else if (this.player2Clicks.includes(2) 
+      && this.player2Clicks.includes(5) 
+      && this.player2Clicks.includes(8))
+      {
+        this.msg = 'PLAYER 2 Won'
+      }
+
+
+      else if (this.player2Clicks.includes(0) 
+      && this.player2Clicks.includes(4) 
+      && this.player2Clicks.includes(8))
+      {
+        this.msg = 'PLAYER 2 Won'
+      }
+      else if (this.player2Clicks.includes(2) 
+      && this.player2Clicks.includes(4) 
+      && this.player2Clicks.includes(6))
+      {
+        this.msg = 'PLAYER 2 Won'
+      }
+    }
   }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@600&display=swap');
+  
+  .app{
+    font-family: 'Quicksand', sans-serif;
+    text-align: center;
+  }
+  .container{
+    width: 340px;
+    height: 330px;
+    background-color: #F2C078;
+
+    padding-top: 10px;
+    margin-left: auto;
+    margin-right: auto;
+
+    border-radius: 5px;
+
+    display: grid;
+    grid-template-columns: repeat(3, 100px);
+    grid-template-rows: repeat(3, 100px);
+    row-gap: 10px;
+    column-gap: 10px;
+
+    text-align: center;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .box{
+    width: 100%;
+    height: 100%;
+    background-color: #FAEDCA;
+
+    border-radius: 5px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+ 
 </style>
---
+++ src/components/Box.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="box" @click.self="test()">
+        {{msg}}
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name:'Box',
+    props: {
+        boxNum: {
+            type: Number,
+            required: true
+        },
+
+        round: Number
+    },
+
+    data: function(){
+        return{
+            msg: '',
+            title: 'FORM CHILD TO PARENT'
+        }
+    },
+
+    methods: {
+        test: function(){
+            
+
+            if(this.round % 2 === 0){
+                this.msg = 'X'
+            }
+            else{
+                this.msg = 'O'
+            }
+
+
+            this.$emit('boxClicked', this.boxNum)
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+    .box{
+        font-size: 4em;
+    }
+
+</style>
